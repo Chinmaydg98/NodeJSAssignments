b4dddb476a25915391bb08e885e91201f3538f9a
Chinmaydg98 <chiinmaydgawande1998@gmail.com>
2022-02-15 18:13:24 +0530

    Added user-list route. Added headers for each response. Minor fixes

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -5,16 +5,37 @@ const server = http.createServer((req, res) => {
     //Homepage
     if (req.url === '/') {
         //Simple form which submits a username.
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<title>Home</title>');
         res.write('<body>');
         res.write('<form action="/create-user" method="POST">');
         res.write('<input type="text" name="username" placeholder="Username"></input>');
-        res.write('<button type="submit">Add user</button');
+        res.write('<button type="submit">Add user</button>');
         res.write('</form>');
+        res.write('<a href="/users">Show All Users</a>')
         res.write('</body>');
         res.write('</html>');
-        res.end();
+        return res.end();
+    }
+
+    //User list
+    if(req.url === '/users'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<title>Users</title>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>Mike Hunt</li>');
+        res.write('<li>Ben Dover</li>');
+        res.write('<li>Mike Oxlong</li>');
+        res.write('<li>Jenny Talls</li>');
+        res.write('<li>Moe Lester</li>');
+        res.write('</ul>');
+        res.write('<a href="/">Back</a>')
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
     }
 
     //User creation
@@ -33,8 +54,9 @@ const server = http.createServer((req, res) => {
             
             //Simply console log username and redirect to home page. 
             console.log(username);
+            res.statusCode = 302;
             res.setHeader('Location', '/');
-            res.end();
+            return res.end();
         })
     }
 });
