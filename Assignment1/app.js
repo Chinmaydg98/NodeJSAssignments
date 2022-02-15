8deed8895b996a866ef8bc8fd32050aff5a334bd
Chinmaydg98 <chiinmaydgawande1998@gmail.com>
2022-02-15 17:57:16 +0530

    User creation, minor fixes

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+
+    //Homepage
     if (req.url === '/') {
         //Simple form which submits a username.
         res.write('<html>');
@@ -8,21 +10,32 @@ const server = http.createServer((req, res) => {
         res.write('<body>');
         res.write('<form action="/create-user" method="POST">');
         res.write('<input type="text" name="username" placeholder="Username"></input>');
-        res.write('<button name="submit">Add user</button');
+        res.write('<button type="submit">Add user</button');
         res.write('</form>');
         res.write('</body>');
         res.write('</html>');
         res.end();
     }
+
+    //User creation
     if (req.url === '/create-user') {
-        //Simply console logs username from home page. 
-        res.write('<html>');
-        res.write('<title>User creation</title>');
-        res.write('<body>');
-        res.write('<h1>Reached user-create</h1>');
-        res.write('</body>');
-        res.write('</html>');
-        res.end();
+        const formData = []
+        
+        //Fetch data
+        req.on('data', (chunk) => {
+            formData.push(chunk);
+        })
+
+        //Parse fetched data
+        req.on('end', () => {
+            const parsedFormData = Buffer.concat(formData).toString();
+            const username = parsedFormData.split('=')[1];
+            
+            //Simply console log username and redirect to home page. 
+            console.log(username);
+            res.setHeader('Location', '/');
+            res.end();
+        })
     }
 });
 
